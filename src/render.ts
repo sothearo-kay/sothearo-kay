f10d8d06be358986c0f4f9a7d42e7f15e048458c
sothearo-kay <sothearo.kay@techostartup.center>
2024-11-06 08:39:59 +0700

    fix(*): fixed readme

--- src/render.ts
+++ src/render.ts
@@ -2,7 +2,7 @@ import type { Year } from './worker';
 
 const BP_MEDIUM = 550;
 const BP_LARGE = 700;
-const BODY_COPY = `I'm Sothearo, A web developer. I create engaging web applications with modern tech. I like reading manga 📓.`;
+const BODY_COPY = `I'm Sothearo, A web developer. I create engaging web applications with modern tech. I love reading manga.`;
 
 interface Props {
   width?: number;
